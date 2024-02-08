c582f20e88a783622e4e0841b9e631d4b8b5f81a
MazenEllithy98 <mazen.a.ellethy@gmail.com>
2024-02-08 14:32:59 +0200

    adding not found component to home, cart ,food pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,9 @@ import { FormsModule } from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
 import { FoodPageComponent } from './food-page/food-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent, CartPageComponent ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent, CartPageComponent, NotFoundComponent ],
   imports: [BrowserModule, AppRoutingModule , FormsModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
